661706072353f9430c89b45959d41f68191211be
Maisara Waseem <108880763+MissMais@users.noreply.github.com>
2026-02-21 00:14:06 +0530

    Update Forgotpass.jsx

--- src/Components/Forgotpass.jsx
+++ src/Components/Forgotpass.jsx
@@ -48,7 +48,7 @@ export default function ForgotPassword() {
               {...register("to", { required: "email is required" })}
               type="email"
               id="email"
-              placeholder="Enter your email"
+              placeholder="Enter registered email"
               className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             {errors.email && (
